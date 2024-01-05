d15254e88d267df8b57b920deb479f2c799a462a
Mstsislau <mr.chadway@gmail.com>
2024-01-05 15:01:28 +0300

    rename version ci python

--- nodb_fastapi/settings.py
+++ nodb_fastapi/settings.py
@@ -2,18 +2,12 @@
     Settings
 """
 
-from pathlib import Path
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
     """ Settings class """
-    main_url: str = ""
-
-    model_config = SettingsConfigDict(
-        # ищем файл .env в директории /src/
-        env_file=f"{Path(__file__).resolve().parent}/.env"
-    )
+    main_url: str
 
 
 settings = Settings()
